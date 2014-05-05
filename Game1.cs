80ecabe65ac9658ffdcc0c81bac8174f86c6e7dc
Rocky Quinn <raq5889@rit.edu>
2014-05-05 02:58:57 -0400

    Update Game1.cs
    Added more organization, and a couple new Dictionaries to be used at a later date.

--- Game1.cs
+++ Game1.cs
@@ -20,9 +20,11 @@ namespace Pikmin_4
         SpriteBatch spriteBatch;
 
         /// <summary>
-        /// contains the state of the game and what panel is currently displayed
+        /// Contains the state of the game and what panel is currently displayed:
+        ///     "main": Title screen.
+        ///     "game": Game screen.
         /// </summary>
-        String baseState;
+        public static String baseState;
 
         /// <summary>
         /// Used to maintain the keys pressed.
@@ -37,12 +39,27 @@ namespace Pikmin_4
         /// <summary>
         /// Position handler of mouse.
         /// </summary>
-        private Cursor cursor = new Cursor();
+        private Cursor cursor;
+
+        /// <summary>
+        /// Button that when clicked will change the game state to "game"
+        /// </summary>
+        private Button playButton;
+
+        /// <summary>
+        /// Dictionary of all objects that can have collisions
+        /// </summary>
+        public static Dictionary<String, Object> COLLISIONS;
 
         /// <summary>
         /// Dictionary of all images used in the title screen
         /// </summary>
         public static Dictionary<String, Texture2D> TITLE_IMAGES;
+
+        /// <summary>
+        /// Dictionary of all images used for buttons
+        /// </summary>
+        public static Dictionary<String, Texture2D> BUTTON_IMAGES;
 
         /// <summary>
         /// Dictionary of all images used for the pikmin
@@ -88,11 +105,14 @@ namespace Pikmin_4
             graphics.PreferredBackBufferWidth = 800;
 
             TITLE_IMAGES = new Dictionary<String, Texture2D>();
+            BUTTON_IMAGES = new Dictionary<String, Texture2D>();
             PIKMIN_IMAGES = new Dictionary<String, Texture2D>();
             PLAYER_IMAGES = new Dictionary<String, Texture2D>();
-            ENEMY_IMAGES = new Dictionary<string, Texture2D>();
-            BACKGROUND_IMAGES = new Dictionary<string, Texture2D>();
-            FOREGROUND_IMAGES = new Dictionary<string, Texture2D>();
+            ENEMY_IMAGES = new Dictionary<String, Texture2D>();
+            BACKGROUND_IMAGES = new Dictionary<String, Texture2D>();
+            FOREGROUND_IMAGES = new Dictionary<String, Texture2D>();
+
+            COLLISIONS = new Dictionary<string, object>();
 
             kbState = new KeyboardState();
             mState = new MouseState();
@@ -102,8 +122,7 @@ namespace Pikmin_4
         }
 
         /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
+        /// Called at the beginning of the game. Loads stuff out of Content folder.
         /// </summary>
         protected override void LoadContent()
         {
@@ -112,15 +131,13 @@ namespace Pikmin_4
             // Adds all the images for the title screen to this dictionary.
             TITLE_IMAGES.Add("background", Content.Load<Texture2D>("title/background"));
             TITLE_IMAGES.Add("title", Content.Load<Texture2D>("title/title"));
-            TITLE_IMAGES.Add("cursor", Content.Load<Texture2D>("title/Title_Cursor"));
             TITLE_IMAGES.Add("pikminFamily", Content.Load<Texture2D>("title/Pikmin_Family"));
-            TITLE_IMAGES.Add("blueLeaf", Content.Load<Texture2D>("title/Leaf_Blue_Pikmin_Front"));
-            TITLE_IMAGES.Add("redLeaf", Content.Load<Texture2D>("title/Leaf_Red_Pikmin_Front"));
-            TITLE_IMAGES.Add("yellowLeaf", Content.Load<Texture2D>("title/Leaf_Yellow_Pikmin_Front"));
-            TITLE_IMAGES.Add("whiteLeaf", Content.Load<Texture2D>("title/Leaf_White_Pikmin_Front"));
-            TITLE_IMAGES.Add("purpleLeaf", Content.Load<Texture2D>("title/Leaf_Purple_Pikmin_Front"));
-            TITLE_IMAGES.Add("rockLeaf", Content.Load<Texture2D>("title/Leaf_Rock_Pikmin_Front"));
-            TITLE_IMAGES.Add("flyingLeaf", Content.Load<Texture2D>("title/Leaf_Flying_Pikmin_Front"));
+
+            // Adds all the images for buttons
+            BUTTON_IMAGES.Add("play", Content.Load<Texture2D>("Buttons/Play_Button"));
+            playButton = new Button(350, 300, BUTTON_IMAGES["play"].Width, BUTTON_IMAGES["play"].Height,
+                                    "game", BUTTON_IMAGES["play"]);
+            COLLISIONS.Add("playButton", playButton);
 
             // Adds all the images for playable characters to this dictionary.
             PLAYER_IMAGES.Add("olimarRight1", Content.Load<Texture2D>("PlayerAnimation/olimar_right_walk1"));
@@ -128,6 +145,14 @@ namespace Pikmin_4
             PLAYER_IMAGES.Add("olimarRight3", Content.Load<Texture2D>("PlayerAnimation/olimar_right_walk3"));
 
             // Adds all the images for pikmin to this dictionary.
+            PIKMIN_IMAGES.Add("blueLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_Blue_Pikmin_Front"));
+            PIKMIN_IMAGES.Add("redLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_Red_Pikmin_Front"));
+            PIKMIN_IMAGES.Add("yellowLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_Yellow_Pikmin_Front"));
+            PIKMIN_IMAGES.Add("whiteLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_White_Pikmin_Front"));
+            PIKMIN_IMAGES.Add("purpleLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_Purple_Pikmin_Front"));
+            PIKMIN_IMAGES.Add("rockLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_Rock_Pikmin_Front"));
+            PIKMIN_IMAGES.Add("flyingLeafFront", Content.Load<Texture2D>("PikminAnimation/Leaf_Flying_Pikmin_Front"));
+
             PIKMIN_IMAGES.Add("blueLeafRight1", Content.Load<Texture2D>("PikminAnimation/blue_leaf_walk_1"));
             PIKMIN_IMAGES.Add("blueLeafRight2", Content.Load<Texture2D>("PikminAnimation/blue_leaf_walk_2"));
             PIKMIN_IMAGES.Add("blueLeafRight3", Content.Load<Texture2D>("PikminAnimation/blue_leaf_walk_3"));
@@ -167,6 +192,21 @@ namespace Pikmin_4
             PIKMIN_IMAGES.Add("whiteFlowerRight1", Content.Load<Texture2D>("PikminAnimation/white_flower_walk_1"));
             PIKMIN_IMAGES.Add("whiteFlowerRight2", Content.Load<Texture2D>("PikminAnimation/white_flower_walk_2"));
             PIKMIN_IMAGES.Add("whiteFlowerRight3", Content.Load<Texture2D>("PikminAnimation/white_flower_walk_3"));
+
+
+            //Adds images for enemies and animations for enemies.
+            //ENEMY_IMAGES.Add("|_Enemy_Image_Names_|", Content.Load<Texture2D>("|_Content_Location_|"));
+
+
+            //Adds foreground images and images that have collision.
+            FOREGROUND_IMAGES.Add("gameCursor", Content.Load<Texture2D>("Cursors/Game_Cursor"));
+            FOREGROUND_IMAGES.Add("titleCursor", Content.Load<Texture2D>("Cursors/Title_Cursor"));
+            cursor = new Cursor(FOREGROUND_IMAGES["titleCursor"]);
+            COLLISIONS.Add("cursor", cursor);
+
+
+            //Adds background images and images that dont have collision.
+            //BACKGROUND_IMAGES.Add("|_background images names_|", Content.Load<Texture2D>("|_Content Location_|"));
         }
 
         /// <summary>
@@ -175,8 +215,8 @@ namespace Pikmin_4
         /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
+
 
         /// <summary>
         /// Allows the game to run logic such as updating the world,
@@ -194,9 +234,12 @@ namespace Pikmin_4
             mState = Mouse.GetState();
             kbState = Keyboard.GetState();
             cursor.update();
+            CollisionHandler.update();
+
 
             base.Update(gameTime);
         }
+
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -207,12 +250,28 @@ namespace Pikmin_4
             GraphicsDevice.Clear(Color.White);
 
             spriteBatch.Begin();
-
+            
             if(baseState.Equals("main"))
             {
+                if (cursor.getImage() != FOREGROUND_IMAGES["titleCursor"])
+                    cursor.setImage(FOREGROUND_IMAGES["titleCursor"]);
                 spriteBatch.Draw(TITLE_IMAGES["background"], new Vector2(0, 0), Color.White);
                 spriteBatch.Draw(TITLE_IMAGES["title"], new Vector2(100, 25), Color.White);
-                spriteBatch.Draw(TITLE_IMAGES["cursor"], cursor.getPosition(), Color.White);
+
+                if (playButton.isSelected())
+                    spriteBatch.Draw(playButton.getImage(), playButton.getPosition(), Color.Black);
+                else
+                    spriteBatch.Draw(playButton.getImage(), playButton.getPosition(), Color.White);
+
+                spriteBatch.Draw(cursor.getImage(), cursor.getPosition(), Color.White);
+                if (playButton.isClicked())
+                    baseState = "game";
+            }
+            else if (baseState.Equals("game"))
+            {
+                if (cursor.getImage() != FOREGROUND_IMAGES["gameCursor"])
+                    cursor.setImage(FOREGROUND_IMAGES["gameCursor"]);
+                spriteBatch.Draw(cursor.getImage(), cursor.getPosition(), Color.White);
             }
 
             spriteBatch.End();
