27398b0c760f622db80351c70115cb90ab9873a2
Rocky Quinn <raq5889@rit.edu>
2014-05-05 03:03:25 -0400

    Update Button.cs
    Button is now fully functional as far as I can tell. I may remove states when in later development due to the way my collision class works.

--- Button.cs
+++ Button.cs
@@ -11,35 +11,125 @@ namespace Pikmin_4
     class Button
     {
         private int x, y, width, height;
+        /// <summary>
+        /// 'x' and 'y' coordinate for this button.
+        /// </summary>
         private Vector2 position;
-        private Texture2D background,foreground;
+        /// <summary>
+        /// Image representation of the button.
+        /// </summary>
+        private Texture2D image;
+        /// <summary>
+        /// String representation of the state to be switched to.
+        /// </summary>
+        private String stateChange;
+        /// <summary>
+        /// Boolean representing whether or not button is selected.
+        /// </summary>
+        private bool selected;
+        /// <summary>
+        /// Boolean representing whether or not button is clicked.
+        /// </summary>
+        private bool clicked;
 
-        public Button(int nx, int ny, int w, int h, Texture2D fore, Texture2D back)
+        /// <summary>
+        /// Constructor for a Button.
+        /// </summary>
+        /// <param name="nx">(int) X position</param>
+        /// <param name="ny">(int) Y position</param>
+        /// <param name="w">(int) Width</param>
+        /// <param name="h">(int) Height</param>
+        /// <param name="state">(String) State to change to if button is clicked</param>
+        /// <param name="im">(Texture2D) Image</param>
+        public Button(int nx, int ny, int w, int h, String state, Texture2D im)
         {
             x = nx;
             y = ny;
             width = w;
             height = h;
+            selected = false;
+            clicked = false;
+            stateChange = state;
 
             position = new Vector2(x, y);
-            background = back;
-            foreground = fore;
+            image = im;
         }
 
-
+        /// <summary>
+        /// Returns the position of the Button.
+        /// </summary>
+        /// <returns>(Vector2) position</returns>
         public Vector2 getPosition()
         {
             return position;
         }
 
+        /// <summary>
+        /// Returns the image of the button.
+        /// </summary>
+        /// <returns>(Texture2D) image</returns>
+        public Texture2D getImage()
+        {
+            return image;
+        }
+
+        /// <summary>
+        /// Returns the width of the button.
+        /// </summary>
+        /// <returns>(int) width</returns>
         public int getWidth()
         {
             return width;
         }
 
+        /// <summary>
+        /// Returns the height of the Button
+        /// </summary>
+        /// <returns>(int) height</returns>
         public int getHeight()
         {
             return height;
+        }
+
+        /// <summary>
+        /// Returns whether the button is selected
+        /// </summary>
+        /// <returns>(bool) selected</returns>
+        public bool isSelected()
+        {
+            return selected;
+        }
+
+        /// <summary>
+        /// Forces the button to be selected
+        /// </summary>
+        public void forceSelect()
+        {
+            selected = true;
+        }
+
+        /// <summary>
+        /// Makes the button unselected
+        /// </summary>
+        public void unselect()
+        {
+            selected = false;
+        }
+
+        /// <summary>
+        /// Sets clicked to true
+        /// </summary>
+        /// <returns>(String) stateChange</returns>
+        public String click()
+        {
+            clicked = true;
+            return stateChange;
+        }
+
+
+        public bool isClicked()
+        {
+            return clicked;
         }
     }
 }
