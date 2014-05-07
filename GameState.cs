12633ef738da1c3f9a282059b1772481ccec239e
Rocky Quinn <raq5889@rit.edu>
2014-05-07 12:08:41 -0400

    Update GameState.cs
    Added pikmin to objects that can have collision

--- GameState.cs
+++ GameState.cs
@@ -30,6 +30,7 @@ namespace Pikmin_4
             allPikmin.Add(new Pikmin("red", 3, 0, 100));
             allPikmin.Add(new Pikmin("yellow", 3, 0, 200));
             allPikmin.Add(new Pikmin("white", 3, 0, 300));
+            Game1.COLLISIONS.Add("pikminList", allPikmin);
             isInitiated = true;
         }
 
