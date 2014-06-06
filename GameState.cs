cace97ae70f4740f4a2412eecfcd9d770c2d378c
Rocky Quinn <raq5889@rit.edu>
2014-06-06 18:38:46 -0400

    movement is done by keyboard now

--- GameState.cs
+++ GameState.cs
@@ -28,9 +28,9 @@ namespace Pikmin_4
         public static void initiate(Cursor c)
         {
             allPikmin = new List<Pikmin>();
-            allPikmin.Add(new Pikmin("blue", 3, 0, 0));
-            allPikmin.Add(new Pikmin("white", 3, 0, 100));
-            allPikmin.Add(new Pikmin("white", 1, 0, 100));
+            allPikmin.Add(new Pikmin("blue", 3, 500, 300));
+            allPikmin.Add(new Pikmin("white", 3, 400, 300));
+            allPikmin.Add(new Pikmin("white", 1, 300, 300));
             Game1.COLLISIONS.Add("pikminList", allPikmin);
 
             cursor = c;
@@ -45,10 +45,39 @@ namespace Pikmin_4
         {
             if (!isInitiated)
                 return;
-            Vector2 loc = new Vector2(Game1.mState.X,Game1.mState.Y);
+            //Vector2 loc = new Vector2(Game1.mState.X,Game1.mState.Y);
+            string direction = "none";
+            if (Game1.kbState.IsKeyDown(Keys.D))
+            {
+                direction = "east";
+                if (Game1.kbState.IsKeyDown(Keys.W))
+                    direction = "northeast";
+                if (Game1.kbState.IsKeyDown(Keys.S))
+                    direction = "southeast";
+                if (Game1.kbState.IsKeyDown(Keys.A))
+                    direction = "none";
+            }
+            else if (Game1.kbState.IsKeyDown(Keys.W))
+            {
+                direction = "north";
+                if (Game1.kbState.IsKeyDown(Keys.A))
+                    direction = "northwest";
+                if (Game1.kbState.IsKeyDown(Keys.S))
+                    direction = "none";
+            }
+            else if (Game1.kbState.IsKeyDown(Keys.S))
+            {
+                direction = "south";
+                if (Game1.kbState.IsKeyDown(Keys.A))
+                    direction = "southwest";
+            }
+            else if (Game1.kbState.IsKeyDown(Keys.A))
+            {
+                direction = "west";
+            }
             for(int i=0; i<allPikmin.Count(); i++)
             {
-                allPikmin[i].moveTowards(loc);
+                allPikmin[i].move(direction);
             }
         }
 
