636a237c12379691e5259f06d29d225d0bd35fab
Rocky Quinn <raq5889@rit.edu>
2014-06-13 23:25:21 -0400

    Update GameState.cs

--- GameState.cs
+++ GameState.cs
@@ -28,10 +28,23 @@ namespace Pikmin_4
         public static void initiate(Cursor c)
         {
             allPikmin = new List<Pikmin>();
-            allPikmin.Add(new Pikmin("blue", 3, 500, 300));
-            allPikmin.Add(new Pikmin("white", 3, 400, 300));
-            allPikmin.Add(new Pikmin("white", 1, 300, 300));
+            allPikmin.Add(new Pikmin("blue", 3, 300, 300));
+            allPikmin.Add(new Pikmin("blue", 2, 310, 310));
+            allPikmin.Add(new Pikmin("blue", 1, 320, 320));
+            allPikmin.Add(new Pikmin("red", 3, 330, 330));
+            allPikmin.Add(new Pikmin("red", 2, 340, 340));
+            allPikmin.Add(new Pikmin("red", 1, 350, 350));
+            allPikmin.Add(new Pikmin("yellow", 3, 360, 360));
+            allPikmin.Add(new Pikmin("yellow", 2, 370, 370));
+            allPikmin.Add(new Pikmin("yellow", 1, 380, 380));
+            allPikmin.Add(new Pikmin("white", 3, 390, 390));
+            allPikmin.Add(new Pikmin("white", 2, 400, 400));
+            allPikmin.Add(new Pikmin("white", 1, 410, 410));
+
+            Character ch = new Character(100, 100, "olimar");
+
             Game1.COLLISIONS.Add("pikminList", allPikmin);
+            Game1.COLLISIONS.Add("character", ch);
 
             cursor = c;
 
@@ -45,7 +58,38 @@ namespace Pikmin_4
         {
             if (!isInitiated)
                 return;
-            //Vector2 loc = new Vector2(Game1.mState.X,Game1.mState.Y);
+
+            if (Game1.kbState.IsKeyDown(Keys.Q))
+            {
+                for (int i = 0; i < allPikmin.Count(); i++)
+                {
+                    allPikmin[i].reanimate();
+                    if (allPikmin[i].getType().Equals("yellow"))
+                        allPikmin[i].moveTowards(new Vector2(200, 200));
+                    else if (allPikmin[i].getType().Equals("red"))
+                        allPikmin[i].moveTowards(new Vector2(300, 100));
+                    else if (allPikmin[i].getType().Equals("blue"))
+                        allPikmin[i].moveTowards(new Vector2(400, 200));
+                    else if (allPikmin[i].getType().Equals("white"))
+                        allPikmin[i].moveTowards(new Vector2(300, 300));
+                }
+            }
+            if (Game1.kbState.IsKeyDown(Keys.L))
+            {
+                for (int i = 0; i < allPikmin.Count(); i++)
+                    allPikmin[i].levelUp();
+            }
+            if (Game1.kbState.IsKeyDown(Keys.K))
+            {
+                for (int i = 0; i < allPikmin.Count(); i++)
+                    allPikmin[i].levelDown();
+            }
+            if (Game1.kbState.IsKeyDown(Keys.J))
+            {
+                for (int i = 0; i < allPikmin.Count(); i++)
+                    allPikmin[i].moveTowards(new Vector2(cursor.getX(), cursor.getY()));
+            }
+
             string direction = "none";
             if (Game1.kbState.IsKeyDown(Keys.D))
             {
@@ -79,6 +123,7 @@ namespace Pikmin_4
             {
                 allPikmin[i].move(direction);
             }
+            ((Character)Game1.COLLISIONS["character"]).move(direction);
         }
 
         /// <summary>
@@ -88,6 +133,7 @@ namespace Pikmin_4
         {
             if (!isInitiated)
                 return;
+            ((CollisionObject)Game1.COLLISIONS["character"]).draw(spriteBatch);
             for (int i = 0; i < allPikmin.Count(); i++)
             {
                 allPikmin[i].draw(spriteBatch);
