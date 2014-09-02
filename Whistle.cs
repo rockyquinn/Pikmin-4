e06aaae206c6b22182089f972828ef55c874f521
Rocky Quinn <raq5889@rit.edu>
2014-09-02 16:31:59 -0400

    Not finished yet

--- Whistle.cs
+++ Whistle.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Pikmin_4
 {
@@ -10,9 +11,20 @@ namespace Pikmin_4
         public Whistle()
             : base("Whistle", GameState.cursor.getX(), GameState.cursor.getX())
         {
-
+            Dictionary<String,Texture2D> aniDict = new Dictionary<String,Texture2D>();
+            aniDict.Add("animation1", Game1.PLAYER_IMAGES["whistle1"]);
+            aniDict.Add("animation2", Game1.PLAYER_IMAGES["whistle2"]);
+            aniDict.Add("animation3", Game1.PLAYER_IMAGES["whistle3"]);
+            aniDict.Add("stand", Game1.PLAYER_IMAGES["whistle1"]);
+            this.setAnimations(aniDict);
+            this.unanimate();
         }
 
 
+        public void whistle(int nx, int ny)
+        {
+            this.setPosition(GameState.cursor.getX(), GameState.cursor.getY());
+            this.reanimate();
+        }
     }
 }
